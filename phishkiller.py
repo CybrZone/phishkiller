90c4b7ca961c3c53b5d18c037018507737f129ea
B1GBOOM <bingleyfred@gmail.com>
2024-07-01 12:12:43 -0400

    email host list | password randomness added

--- phishkiller.py
+++ phishkiller.py
@@ -3,34 +3,39 @@ import requests
 import random
 import string
 import names
-import subprocess
+from emailHosts import email_domains
 
 from fake_useragent import UserAgent
 
 
-
-
-def name_gen():#Generates a random name for the email
-    name_system = random.choice(["FullName", "FullFirstFirstInitial", "FirstInitialFullLast"])
+def name_gen():  # Generates a random name for the email
+    name_system = random.choice(
+        ["FullName", "FullFirstFirstInitial", "FirstInitialFullLast"]
+    )
     first_name = names.get_first_name()
     last_name = names.get_last_name()
-    if name_system == "FullName":#JohnDoe
+    if name_system == "FullName":  # JohnDoe
         return first_name + last_name
-    elif name_system == "FullFirstFirstInitial":#JohnD
+    elif name_system == "FullFirstFirstInitial":  # JohnD
         return first_name + last_name[0]
-    return first_name[0] + last_name#JDoe
+    return first_name[0] + last_name  # JDoe
 
-def generate_random_email():
+
+def generate_random_email():  # Generate email with combination of name and domain
     name = name_gen()
-    NumberOrNo=random.choice(["Number", "No"])
-    domain = random.choice(["@gmail.com", "@yahoo.com", "@rambler.ru", "@protonmail.com", "@outlook.com", "@itunes.com"])#Popular email providers
+    NumberOrNo = random.choice(["Number", "No"])
+    domain = random.choice(email_domains)  # 140+ domains
     if NumberOrNo == "Number":
         return name + str(random.randint(1, 100)) + domain
     else:
         return name + domain
 
-def generate_random_password():
-    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+
+def generate_random_password():  # Generate password using uppercase, lowercase, numbers and special characters
+    characters = string.ascii_letters + string.digits + "@$!%^&*"
+    length = random.randint(12, 20)  # Random length between 12 and 20
+    return "".join(random.choice(characters) for _ in range(length))
+
 
 def send_posts(url):
     while True:
@@ -39,19 +44,29 @@ def send_posts(url):
         data = {"a": email, "az": password}
         ua = UserAgent()
         user_agent = ua.random
-        headers = {'User-Agent': user_agent}
-        response = requests.post(url, data=data, headers=headers,)
-        print(f"Email: {email}, Password: {password}, Status Code: {response.status_code}, headers: {user_agent}")
+        headers = {"User-Agent": user_agent}
+        response = requests.post(
+            url,
+            data=data,
+            headers=headers,
+        )
+        print(
+            f"Email: {email}, Password: {password}, Status Code: {response.status_code}, headers: {user_agent}"
+        )
+
 
 def main():
     url = input("Enter the URL of the target you want to flood: ")
-    threads = [threading.Thread(target=send_posts, args=(url,), daemon=True) for _ in range(25)]
+    threads = [
+        threading.Thread(target=send_posts, args=(url,), daemon=True) for _ in range(25)
+    ]
 
     for t in threads:
         t.start()
 
     for t in threads:
         t.join()
+
 
 if __name__ == "__main__":
     main()
