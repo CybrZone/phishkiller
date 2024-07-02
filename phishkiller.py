a603f47f68025de14e9e947b7eb40100a946c85b
Leonard <s_xsipo6@uni-bremen.de>
2024-07-02 17:40:16 +0200

    add error handling, add logging

--- phishkiller.py
+++ phishkiller.py
@@ -6,12 +6,14 @@ from typing import List
 from faker import Faker
 from itertools import permutations
 from fake_useragent import UserAgent
+from loguru import logger
 
 
 alphabet = string.ascii_letters + string.digits + string.punctuation
 domains = ['@googlemail.com', "@gmail.com", "@yahoo.com", "@rambler.ru", "@protonmail.com", "@outlook.com", "@itunes.com"]
 MAX_THREADS = 8 # Multithreading is not real in python, therefore there's barely any gain from more
 fake = Faker()
+user_agent = UserAgent()
 
 
 def add_suffixes(permutated: List[str], suffixes: List[str], service: str) -> List[str]:
@@ -61,7 +63,7 @@ def generate_random_password(max_len: int = 50) -> str:
 
 
 def send_posts(url, location):
-    global USER_AGENTS
+    global user_agent
     while True:
         emails = generate_random_emails()
         for email in emails:
@@ -70,27 +72,39 @@ def send_posts(url, location):
                 "a": email,
                 "az": password
             }
-            user_agent = UserAgent()
             headers = {
-                'User-Agent': user_agent
+                'User-Agent': user_agent.random
             }
-            response = requests.post(url, headers=headers, data=data)
-            # Dont comment in during execution. Console logs require locking, slowing down multithreading.
-            #print(f"Email: {email}, Password: {password}, Status Code: {response.status_code}")
-
+            try:
+                response = requests.post(url, headers=headers, data=data)
+                if response.status_code != 200:
+                    logger.error(f'{response.status_code} [::] Error sending message. Payload: <{email}> \"{password}\"')
+                    logger.error(response.text)
+                    return # kill thread
+                logger.info(f'{response.status_code} [::] Sent! Credentials: <{email}> \"{password}\"')
+            except Exception as e:
+                logger.error('[ERROR] Error sending request!', e)
+                return # kill thread
 
 def main():
     global MAX_THREADS
-    URL = input("Enter the URL of the target you want to flood: ")
+    print(f'{"="*20} phishkiller by CybrZone {"="*20}')
+    print()
+    print("Enter the URL of the target you want to flood!")
+    URL = input('#> ')
+    print()
     threads = []
     locations = ["en_US", "en_GB", "it_IT", "en_IE"]
 
-    for i in range(MAX_THREADS):
+    print(f'Starting phishkiller with {MAX_THREADS} threads...')
+    for i in range(1, MAX_THREADS + 1):
+        print(f'Starting thread {i}...', end='\r')
         location = random.choice(locations)
         t = threading.Thread(target=send_posts, args=(URL, location))
         t.daemon = True
         threads.append(t)
-
+        print(f'Started thread {i}.{" "*50}', end='\r')
+    print('\n\n') # Clear line from \r
     for thread in threads:
         thread.start()
 
