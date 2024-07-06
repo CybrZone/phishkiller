0ad51e865cb1d4cc74cdb2742f36dc5c4385ae0a
R <email@example.com>
2024-07-06 13:09:19 +0800

    improved to consume less proxy api calls

--- proxies.py
+++ proxies.py
@@ -7,30 +7,57 @@ configured in secrets.json, file should contain:
 
 api is at rapidapi: https://rapidapi.com/almann/api/proxy-list2
 free version allows up to 1000 requests per month (~30/day)
+
+improved: only request proxies if proxies.json is not the same country and date when checking to run - consumes less api uses
 """
 
 import requests
 import json
+import os
+from datetime import datetime
 
 def get_proxies():
     with open("secrets.json","r") as f:
-        secrets = json.load(f)
-        proxy_api = secrets["proxy_api"]
-        country = secrets["country"]
+        data = json.load(f)
+        proxy_api = data["proxy_api"]
+        country = data["country"]
 
-    url = "https://proxy-list2.p.rapidapi.com/proxy/get"
+    current_date = datetime.now().strftime("%Y%m%d")  # YYYYMMDD string format
+    new_proxies_required = True  # check if new proxy list from api is required
+    if os.path.isfile("proxies.json"):
+        with open("proxies.json","r") as f:
+            data = json.load(f)
+            existing_country = data["country"]
+            date_created = data["date"]
+            if existing_country == country and date_created == current_date:
+                proxies = data["proxies"]  # retrieve generated list of proxies
+                new_proxies_required = False
+                print("using existing proxies.json file")
 
-    querystring = {"type":"http","country":country,"anonymity":"high"}
+    if new_proxies_required == True:
+        url = "https://proxy-list2.p.rapidapi.com/proxy/get"
 
-    headers = {
-        "x-rapidapi-key": proxy_api,
-        "x-rapidapi-host": "proxy-list2.p.rapidapi.com"
-    }
+        querystring = {"type":"http","country":country,"anonymity":"high"}
 
-    response = requests.get(url, headers=headers, params=querystring)
+        headers = {
+            "x-rapidapi-key": proxy_api,
+            "x-rapidapi-host": "proxy-list2.p.rapidapi.com"
+        }
 
-    proxies = response.json()
-    # TODO: check all proxies work and return filtered results accordingly
+        response = requests.get(url, headers=headers, params=querystring)
+
+        proxies = response.json()
+        # TODO: check all proxies work and return filtered results accordingly
+
+        # save proxies to file
+        new_proxy_dict = {
+            "country":country,
+            "date":current_date,
+            "proxies":proxies
+        }
+        print("creating new proxies.json file")
+        with open("proxies.json","w", encoding="utf-8") as f:
+            json.dump(new_proxy_dict,f,ensure_ascii=False,indent=4)
 
     return proxies
 
